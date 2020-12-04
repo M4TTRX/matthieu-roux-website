861b741c859cf1d6bb6728f834427d9633d609a0
Matthieu Roux <matthieurouxleoncini@gmail.com>
2020-12-04 03:04:20 -0500

    Created a new design and version for the website

--- src/App.js
+++ src/App.js
@@ -3,52 +3,49 @@ import "./App.scss";
 import CoverPicture from "./components/coverPicture";
 
 function App() {
-  const contactMeSection = creatContactMeSection();
+  const aboutMeSection = createAboutMe();
   const myProjectsSection = createMyProjectsSection();
+  const myResumeSection = createResumeSection();
   return (
     <div className="App">
       <div>
-        <header className="title">Hi, I'm Matthieu</header>
-        <div className="underline-title"> </div>
-        <div className="flexRow">
-          <p className="limited-width">
-            Hi my name is Matthieu Roux, and thanks for taking the time to visit
-            my website! This place is still in construction so I will keep
-            adding more things to it. In the meantime, you can find my contact
-            info
-          </p>
-          <div className="image-container">
-            {CoverPicture()}
-            <div className="yellow-cover-decoration"></div>
-            <div className="black-cover-decoration"></div>
-          </div>
-        </div>
+        <header className="title">
+          Matthieu <br /> Roux{" "}
+        </header>
       </div>
+      {aboutMeSection}
       {myProjectsSection}
-      {contactMeSection}
     </div>
   );
 }
 
-function creatContactMeSection() {
+function createAboutMe() {
   return (
     <div>
-      {createHeader1("Contact Info")}
+      <h1 className="h1">About Me</h1>
       <p>
-        - Email:{" "}
+        I am a 21 year old student at Queen's University who's about to finish
+        his degree in Computer Engineering. I was born in Reunion Island, a
+        French remote department in the Indian Ocean, my father's work ahs
+        pushed our family to move to Romania, then Poland and finally to Canada.
+        This lifestyle taught me to adapt quickly to unknown environment where
+        sometimes I do not even know the local language.
+      </p>
+      <p>
+        -{" "}
         <a href="mailto:matthieurouxleoncini@gmail.com" className="url">
-          matthieurouxleoncini@gmail.com
+          Email
         </a>
-        <br />- Github:{" "}
+        <br />-{" "}
         <a href="https://github.com/M4TTRX" className="url">
-          https://github.com/M4TTRX
+          Github
         </a>
-        <br />- LinkedIn:{" "}
+        <br />-{" "}
         <a
           href="https://www.linkedin.com/in/matthieu-roux-317878153/"
           className="url"
         >
-          https://www.linkedin.com/in/matthieu-roux-317878153/
+          LinkedIn
         </a>
       </p>
     </div>
@@ -58,22 +55,51 @@ function creatContactMeSection() {
 function createMyProjectsSection() {
   return (
     <div>
-      {createHeader1("My Projects")}
+      <h1 className="h1">Portfolio</h1>
+      <h2> Fitly</h2>
       <p>
-        Wonder what I can make? Here's a list of some of the work I have made
-        both for a class and during my free time.
-        <br />
-        Work in progress, coming soon.
+        Fitly is the social way to track your daily workout. The idea came to me
+        when I was unhappy with the time my friend took to write down his
+        progress when weight lifting with him: it was slow and he could not
+        easily track his progress.
       </p>
+      <p>
+        After out he wasn't the only person using notebooks and being
+        unsatisfied with the existing apps that could help them track their
+        progress, I decided to take the matter in my own hands and make my own
+        fitness application, made for people who like to lift weights and keep
+        track of their progress.
+      </p>
+      <p>
+        The app is still work in progress, but you can check out its github
+        repository.
+      </p>
+      <a href="https://github.com/M4TTRX/fitly" className="url">
+        Github Repository
+      </a>
+      <h2> Reading Glasses</h2>
+      <p>
+        Reading glasses (development name) is pair of glasses that helps the
+        visually impaired read. Upon the press of a button, the glasses will
+        read the text that is presented in front of them.
+      </p>
+      <p>
+        Because this project is my capstone project, I cannot share its
+        repository here yet as it would be a violation of academic integrity.
+      </p>
+      <h2>StudyQ</h2>
     </div>
   );
 }
 
-function createHeader1(header) {
+function createResumeSection() {
   return (
     <div>
-      <header className="h1">{header}</header>
-      <div className="underline-h1"></div>
+      <header className="h1">Resume</header>
+      <p>
+        Want to know more about my education and work experience? Take a look at
+        my resume!
+      </p>
     </div>
   );
 }
